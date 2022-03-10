d65109c10d2a0fa28767e61e8b381e4c2aff610f
trinh van dat <trinhvandat90399@gmail.com>
2022-03-10 11:38:17 +0700

    Create project spring boot social login

---
+++ src/test/java/org/aibles/sociallogin/SpringBootSocialLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package org.aibles.sociallogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSocialLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
